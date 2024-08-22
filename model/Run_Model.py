04d79bddb927c663bcd2438a4e01719a86c567d0
vikramChennai <vikram.a.chennai@gmail.com>
2024-08-21 18:06:26 -0700

    :///

---
+++ model/Run_Model.py
@@ -0,0 +1,20 @@
+
+#configure this file to run your model
+import os
+import sys
+
+#import your AI model into this file
+
+
+def run_model():
+    #A Wrapper for your model to do things. 
+
+
+    return "Model Output"
+    
+    
+
+
+
+
+
