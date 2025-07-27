02f9c85a6969592deef749b6f3aa1f3c474fd42d
BelardoA <abelardo@regscale.com>
2025-07-26 22:38:00 -0400

    Reorganize fixture imports in base_resources.py for improved accessibility
    This update modifies the import order in base_resources.py to enhance the accessibility of test fixtures. The GitHub and MongoDB resources are now imported before the shared resources, ensuring that all necessary fixtures are readily available for testing. This change aims to streamline the testing process and improve code organization.

--- Fixtures/base_resources.py
+++ Fixtures/base_resources.py
@@ -4,9 +4,10 @@ Import all fixtures here to make them available to tests.
 """
 
 # Test fixtures
-from Fixtures.Test.shared_resources import *
-from Fixtures.MongoDB.mongo_resources import *
 from Fixtures.Airflow.airflow_resources import *
+from Fixtures.GitHub.github_resources import *
+from Fixtures.MongoDB.mongo_resources import *
+from Fixtures.Test.shared_resources import *
 
 # Future imports for other categories:
 # from .Databricks.clusters import databricks_cluster, databricks_client
